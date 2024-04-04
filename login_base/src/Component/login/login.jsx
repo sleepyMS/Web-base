b80d14d2247ad69d3e61dbbd668079295835d11d
sleepyMS <minseok011017@gmail.com>
2024-04-05 08:49:34 +0900

    loginEvent

--- login_base/src/Component/login/login.jsx
+++ login_base/src/Component/login/login.jsx
@@ -3,6 +3,34 @@ import "boxicons";
 import "./login.css";
 
 function login() {
+  const loginSub = (e) => {
+    e.preventDefault();
+
+    // API
+    let body = {
+      id,
+      password,
+    };
+    axios.post(LOGIN_API_KEY, body).then((res) => {
+      // code = 데이터 상태
+      const code = res.data.code;
+      if (code === 400) {
+        // 로그인 정보가 부족할 때,
+        alert("아이디/패스워드를 입력해 주세요.");
+      } else if (code === 401) {
+        // id가 틀렸을 때,
+        alert("존재하지 않는 id입니다.");
+      } else if (code === 402) {
+        // 비밀번호가 틀렸을때,
+        alert("비밀번호가 일치하지 않습니다.");
+      } else {
+        dispatch(loginUser(res.data.userInfo));
+      }
+    });
+    // 1순위 로그인 버튼을 누르면 클릭이 안되도록.
+    setLoading(true);
+  };
+
   return (
     <div className="wrapper">
       <div className="login-box">
